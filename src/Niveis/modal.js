24c697e32e824d383e71b7662b4ece21d263fbb8
jessicaduarte95 <jessicaduarte.95@gmail.com>
2023-01-28 17:11:44 -0300

    Adicionando a lógica das perguntas

--- src/Niveis/modal.js
+++ src/Niveis/modal.js
@@ -16,6 +16,7 @@ export const ModalNivel = (props) => {
         container: {
             width: '90%',
             height: '60%',
+            justifyContent: 'space-around',
             backgroundColor: '#000929',
             borderRadius: 15,
             borderWidth: 1.5,
@@ -29,9 +30,34 @@ export const ModalNivel = (props) => {
             shadowOpacity: 0.28,
             shadowRadius: 4 
         },
+        buttom: {
+            zIndex: 99,
+            backgroundColor: '#000720',
+            borderRadius: 6,
+            marginRight: 8,
+            padding: 10,
+            borderWidth: 1,
+            width: '50%',
+            borderColor: 'rgba(50,115,220, 0.4)',
+            shadowColor: 'rgba(50,115,220, 0.9)',
+            shadowOffset: {
+                width: 0,
+                height: 2
+            },
+            elevation: 5,
+            shadowOpacity: 0.28,
+            shadowRadius: 4,
+        },
+        textButton: {
+            textAlign: 'center',
+            fontWeight: 'bold',
+            color: '#E5E5E5',
+            fontSize: 17
+        },
     })    
 
     const [firstLevel, setFirstLevel] = useState({})
+    const [perguntaAtual, setPerguntaAtual] = useState(0)
 
     useEffect(() => {
 
@@ -39,8 +65,8 @@ export const ModalNivel = (props) => {
             nivel
         })
         .then((response) => {
-            setFirstLevel(response.data[0]);
-            console.log(response.data);
+            setFirstLevel(response.data);
+            console.log(firstLevel[0].pergunta);
         })
         .catch((error) => {
             console.log(error);
@@ -55,9 +81,31 @@ export const ModalNivel = (props) => {
         animationType='fade'>
            <View style={styles.modalBackGround}>
                <SafeAreaView style={styles.container}>
-                   <View style={{padding: '7%'}}>
-                       <Text style={{color: '#E5E5E5', fontSize: 25}}>Teste</Text>
+                   <View style={{paddingLeft: '7%', paddingTop: '7%'}}>
+                       <Text style={{color: '#E5E5E5', fontSize: 22}}>{firstLevel[perguntaAtual].pergunta}</Text>
                    </View>
+                   <View style={{alignItems: 'flex-end', paddingRight: '8%'}}>
+                       <Text style={{color: '#E5E5E5', fontSize: 18}}>Pergunta {perguntaAtual + 1} / 10</Text>
+                   </View>
+                   <View>
+                        <TouchableOpacity style={{alignItems: 'flex-start', padding: '4%'}}>
+                            <Text style={styles.textButton}>Opção 1</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={{alignItems: 'flex-start', padding: '4%'}}>
+                            <Text style={styles.textButton}>Opção 2</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={{alignItems: 'flex-start', padding: '4%'}}>
+                            <Text style={styles.textButton}>Opção 3</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={{alignItems: 'flex-start', padding: '4%'}}>
+                            <Text style={styles.textButton}>Opção 4</Text>
+                        </TouchableOpacity>
+                   </View>
+                   <View style= {{alignItems: 'flex-end', paddingBottom: '5%', paddingRight: '4%'}}>
+                        <TouchableOpacity style={styles.buttom} activeOpacity={0.7} onPress={() => setPerguntaAtual(perguntaAtual + 1)}>
+                            <Text style={styles.textButton}>Próxima Pergunta</Text>
+                        </TouchableOpacity>
+                    </View>
                </SafeAreaView>
            </View>
        </Modal>
