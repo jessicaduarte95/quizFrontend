e9ef31f64aa06d1b2884f6b3ec9adb456fab3729
jessicaduarte95 <jessicaduarte.95@gmail.com>
2024-05-08 15:33:41 -0300

    refactor: modal de cadastro

--- src/Pages/Principal/Modais/ModalRegister/ModalRegister.jsx
+++ src/Pages/Principal/Modais/ModalRegister/ModalRegister.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import { BasicModal } from '../../../../components/Modal/Modal';
 import { View } from 'react-native';
 import * as S from './style';
@@ -5,11 +6,17 @@ import { TitleModal } from '../../../../components/Text/Text';
 import { Input } from '../../../../components/Input/Input';
 import { useForm } from 'react-hook-form';
 import { CloseSaveButton } from '../../../../components/Button/Button';
+import { Checkbox } from '../../../../components/Checkbox/Checkbox';
 
 export const ModalRegister = props => {
 	const { open, handleClose } = props;
 
+	const [isCheck, setIsCheck] = useState(false);
 	const { handleSubmit, control, reset } = useForm();
+
+	const isChecked = () => {
+		setIsCheck(!isCheck);
+	};
 
 	const onSubmit = data => {
 		handleClose();
@@ -22,7 +29,8 @@ export const ModalRegister = props => {
 			<TitleModal>Cadastrar</TitleModal>
 			<Input name="nome" control={control} placeholder={'Nome'} />
 			<Input name="email" control={control} placeholder={'Email'} />
-			<Input name="senha" control={control} placeholder={'Senha'} secureTextEntry={true} />
+			<Input name="senha" control={control} placeholder={'Senha'} secureTextEntry={!isCheck} />
+			<Checkbox onPress={isChecked} isCheck={isCheck} />
 			<View style={S.ModalRegister.ContainerButton}>
 				<CloseSaveButton
 					onPress={() => {
